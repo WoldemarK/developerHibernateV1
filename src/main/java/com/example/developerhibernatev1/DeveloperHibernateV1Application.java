9860178a4ed3b3304c91294436d1083a375fd994
WoldemarK <kovtynov.vladimir@gmail.com>
2023-10-23 01:45:06 +0300

    корректировка class DeveloperView

--- src/main/java/com/example/developerhibernatev1/DeveloperHibernateV1Application.java
+++ src/main/java/com/example/developerhibernatev1/DeveloperHibernateV1Application.java
@@ -1,7 +1,9 @@
 package com.example.developerhibernatev1;
 
+import com.example.developerhibernatev1.model.Developer;
 import com.example.developerhibernatev1.model.Skill;
 import com.example.developerhibernatev1.model.Specialty;
+import com.example.developerhibernatev1.view.DeveloperView;
 import com.example.developerhibernatev1.view.SkillView;
 import com.example.developerhibernatev1.view.SpecialtyView;
 
@@ -18,16 +20,28 @@ public class DeveloperHibernateV1Application {
 //        specialtyView.specialtyById(1L);
 //        specialtyView.allSpecialty();
 //        specialtyView.deleteSpecialtyById(11L);
-        SkillView skillView = new SkillView();
+//        SkillView skillView = new SkillView();
 //        skillView.deleteSkillById(7L);
 //        skillView.updateSkill(Skill.builder()
-//                        .name("Teskkkkkkting")
+//                .name("Teskkkkkkting")
 //                .build(), 1L);
 //        skillView.allSkills();
 //        skillView.skillById(1L);
-skillView.createSkill(Skill.builder()
-                .name("Test36")
-        .build());
+//        skillView.createSkill(Skill.builder()
+//                .name("Test3")
+//                .build());
+        DeveloperView developerView = new DeveloperView();
+//        developerView.createDeveloper(Developer.builder()
+//                        .firstName("TestF")
+//                        .lastName("TestL")
+//                .build());
+//        developerView.updateDeveloper(Developer.builder()
+//                        .firstName("TestF2")
+//                        .lastName("TestL2")
+//                .build(),8L);
+//        developerView.getByIdDeveloper(122L);
+//        developerView.allDevelopers();
+        developerView.getByIdDeveloper(8L);
 
     }
 
--- src/main/java/com/example/developerhibernatev1/repository/hibernate/DeveloperRepositoryImpl.java
+++ src/main/java/com/example/developerhibernatev1/repository/hibernate/DeveloperRepositoryImpl.java
@@ -7,81 +7,76 @@ import com.example.developerhibernatev1.repository.DeveloperRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.hibernate.Session;
 
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Queue;
 
 import static com.example.developerhibernatev1.util.HibernateSessionFactoryUtil.session;
+
 @RequiredArgsConstructor
 public class DeveloperRepositoryImpl implements DeveloperRepository {
     @Override
     public Optional<Developer> save(Developer developer) {
-        developer = Developer.builder()
-                .firstName(developer.getFirstName())
-                .lastName(developer.getLastName())
-                .build();
-        session()
-                .beginTransaction();
-        session()
-                .persist(developer);
-        session()
-                .getTransaction().commit();
-        return Optional.of(developer);
+        try (Session session = session()) {
+            developer = Developer.builder()
+                    .firstName(developer.getFirstName())
+                    .lastName(developer.getLastName())
+                    .build();
+            session.beginTransaction();
+            Developer createdDeveloper = session.merge(developer);
+            session.getTransaction().commit();
+            return Optional.of(createdDeveloper);
+        }
+
     }
     @Override
     public Optional<Developer> update(Developer developer, Long id) {
-        developer = Developer.builder()
-                .id(id)
-                .firstName(developer.getFirstName())
-                .lastName(developer.getLastName())
-                .build();
-        session()
-                .beginTransaction();
-        session()
-                .saveOrUpdate(developer);
-        session()
-                .getTransaction()
-                .commit();
-        return Optional.of(developer);
+        try (Session session = session()){
+            developer = Developer.builder()
+                    .id(id)
+                    .firstName(developer.getFirstName())
+                    .lastName(developer.getLastName())
+                    .build();
+            session.beginTransaction();
+            session.saveOrUpdate(developer);
+            session.getTransaction().commit();
+            return Optional.of(developer);
+        }
     }
-
     @Override
     public Optional<Developer> getId(Long id) {
-        session()
-                .beginTransaction();
-        Developer developer = session()
-                .get(Developer.class, id);
-        session()
-                .getTransaction()
-                .commit();
-        return Optional.ofNullable(developer);
+        try (Session session = session()) {
+            session.beginTransaction();
+            Optional<Developer> developer = Optional.ofNullable(session.get(Developer.class, id));
+            if (developer.isPresent()) {
+                session.getTransaction().commit();
+                return developer;
+            }
+        }
+        throw new NotFoundException("По данному запросу ID не чего не найдено " + id);
     }
     @Override
     @Transactional
     public List<Developer> getAll() {
-        session()
-                .beginTransaction();
-        List developers = session()
-                .createQuery("from Developer")
-                .list();
+        try (Session session = session()){
+            session.beginTransaction();
+            List<Developer> developers = session.createQuery("from Developer").list();
+            session.getTransaction().commit();
+            return developers;
+        }
 
-        session()
-                .getTransaction()
-                .commit();
-        return developers;
     }
     @Override
     public void deleteById(Long id) {
-        session().beginTransaction();
-        Optional<Developer> developer = Optional.ofNullable(session().get(Developer.class, id));
-        if (developer.isPresent()) {
-            session().remove(developer);
-        } else throw new NotFoundException("По данному запросу ID не найден " + id);
-        session()
-                .getTransaction()
-                .commit();
+        try (Session session = session()){
+            session.beginTransaction();
+            Queue<Developer> developer = (Queue<Developer>) session.createQuery("from Developer d where d.id=:id");
+            session.remove(developer);
+            session.getTransaction().commit();
+        }
     }
-
 }
 
